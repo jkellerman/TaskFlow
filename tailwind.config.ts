0cfefbe621809c8e9fb806aa4279a7a7b0607be3
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-04-29 19:19:47 +0100

    chore: setup themes and global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
+module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +9,48 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        border: "hsl(var(--border-color))",
+        destructive: "hsl(var(--destructive))",
+        text: "hsl(var(--text-color))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          lighter: "hsl(var(--secondary-lighter))",
+          darker: "hsl(var(--secondary-darker))",
+        },
+        tertiary: {
+          DEFAULT: "hsl(var(--tertiary))",
+          lighter: "hsl(var(--tertiary-lighter))",
+          darker: "hsl(var(--tertiary-darker))",
+        },
+        today: {
+          DEFAULT: "hsl(var(--today-text))",
+          background: "hsl(var(--today-background))",
+          border: "hsl(var(--today-border))",
+        },
+        tomorrow: {
+          DEFAULT: "hsl(var(--tomorrow-text))",
+          background: "hsl(var(--tomorrow-background))",
+          border: "hsl(var(--tomorrow-border))",
+        },
+        "future-date": {
+          DEFAULT: "hsl(var(--future-date-text))",
+          background: "hsl(var(--future-date-background))",
+          border: "hsl(var(--future-date-border))",
+        },
+        "past-date": {
+          DEFAULT: "hsl(var(--past-date-text))",
+          background: "hsl(var(--past-date-background))",
+          border: "hsl(var(--past-date-border))",
+        },
       },
     },
   },
   plugins: [],
 };
-export default config;

7b5e7c95edd299aa56e22572e2765ee6a6fcaf56
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-05-06 22:19:59 +0100

    feat: create search component

--- app/(dashboard)/board/page.tsx
+++ app/(dashboard)/board/page.tsx
@@ -1,22 +1,11 @@
 import Icons from "@/components/icons";
 import { Button } from "@/components/ui/button";
+import Search from "@/components/ui/search";
 
 export default function Page() {
 	return (
 		<div className="flex items-center justify-between border-b border-border bg-white px-8 py-5 dark:bg-tertiary">
-			<div className="relative flex max-w-96 flex-1 flex-shrink-0 text-text ">
-				<label htmlFor="search" className="sr-only">
-					Search
-				</label>
-				<input
-					className=" peer block w-full rounded-md border-2 border-border bg-secondary py-[9px] pl-10 text-sm font-medium text-tertiary-darker outline-1 outline-secondary-darker placeholder:text-text dark:bg-tertiary-lighter dark:text-white dark:outline-text"
-					placeholder="Search any task..."
-				/>
-				<Icons
-					icon="Search"
-					className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 transition-colors duration-300 peer-focus:text-tertiary-darker dark:peer-focus:text-white"
-				/>
-			</div>
+			<Search />
 			<div className="flex items-center gap-2">
 				<Button className="gap-2 bg-size-200">
 					Create Task
---
+++ components/ui/search.tsx
@@ -0,0 +1,23 @@
+import Icons from "../icons";
+
+export default function Search() {
+	return (
+		<div className="relative flex max-w-96 flex-1 flex-shrink-0 text-text ">
+			<label htmlFor="search" className="sr-only">
+				Search
+			</label>
+			<input
+				className=" peer block w-full rounded-md border-2 border-border bg-secondary py-[9px] pl-10 pr-2 text-sm font-medium text-tertiary-darker outline-1 outline-secondary-darker placeholder:text-text dark:bg-tertiary-lighter dark:text-white dark:outline-text"
+				placeholder="Search any task..."
+				type="search"
+				name="search"
+				id="search"
+				autoComplete="off"
+			/>
+			<Icons
+				icon="Search"
+				className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 transition-colors duration-300 peer-focus:text-tertiary-darker dark:peer-focus:text-white"
+			/>
+		</div>
+	);
+}

21b0e214d382641c0dfef947186b669d61603e8d
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-05-16 15:49:47 +0100

    refactor: move columns and list props up a level to be used to both board and create task form

--- app/(dashboard)/board/page.tsx
+++ app/(dashboard)/board/page.tsx
@@ -1,24 +1,31 @@
 import Board from "@/components/board";
 import Icons from "@/components/icons";
 import Search from "@/components/search";
-import { Button } from "@/components/ui/button";
+import CreateTaskForm from "@/components/task/create-form";
+import { generateMockData } from "@/data/mock-data";
 
 export default function Page() {
+	const data = generateMockData();
+	const columns = data.boards[0].status;
+	const taskList = columns.flatMap((tasks) => {
+		const color = tasks.color;
+
+		return tasks.tasks.map((task) => {
+			return { ...task, color };
+		});
+	});
 	return (
 		<>
 			<div className="flex items-center justify-between gap-2 border-t border-border bg-white px-4 py-5 transition-colors dark:bg-tertiary sm:gap-0 sm:px-8">
 				<Search />
 				<div className="flex items-center gap-2">
-					<Button className="gap-2 bg-size-200 py-3 sm:py-2">
-						<span className="hidden sm:inline-block">Create Task</span>
-						<Icons icon="Plus" className=" h-3 w-3 hover:bg-pos-100" />
-					</Button>
+					<CreateTaskForm columns={columns} />
 					<button type="button" className="hover:text-tertiary-darker hover:transition-colors dark:hover:text-white">
 						<Icons icon="Ellipsis" className="h-6 w-6" />
 					</button>
 				</div>
 			</div>
-			<Board />
+			<Board columns={columns} taskList={taskList} />
 		</>
 	);
 }
